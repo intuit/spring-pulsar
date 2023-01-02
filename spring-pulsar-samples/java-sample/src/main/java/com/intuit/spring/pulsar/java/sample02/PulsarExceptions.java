7e019405b622cb00a75f479aa25735f7dc71a658
kjindal <kritika_jindal@intuit.com>
2023-01-02 18:15:37 +0530

    removed uneccesary dependencies

--- spring-pulsar-samples/java-sample/src/main/java/com/intuit/spring/pulsar/java/sample02/PulsarExceptions.java
+++ spring-pulsar-samples/java-sample/src/main/java/com/intuit/spring/pulsar/java/sample02/PulsarExceptions.java
@@ -1,14 +1,11 @@
 package com.intuit.spring.pulsar.java.sample02;
 
-import kotlin.reflect.KClass;
-
 class ShortMessageProducerException extends RuntimeException {
-
     ShortMessageProducerException(String message) {
         super(message);
-
     }
 }
+
 class LongMessageConsumerException extends RuntimeException {
     LongMessageConsumerException(String message) {
         super(message);
