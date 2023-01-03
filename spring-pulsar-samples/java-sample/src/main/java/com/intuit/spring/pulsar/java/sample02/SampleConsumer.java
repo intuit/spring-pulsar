e1a9a544c77e4fe17043ef8a75315e44f2f7741f
kritikajin <37775291+kritikajin@users.noreply.github.com>
2023-01-03 09:20:32 +0530

    removed unnecessary dependencies (#18)

--- spring-pulsar-samples/java-sample/src/main/java/com/intuit/spring/pulsar/java/sample02/SampleConsumer.java
+++ spring-pulsar-samples/java-sample/src/main/java/com/intuit/spring/pulsar/java/sample02/SampleConsumer.java
@@ -4,7 +4,6 @@ import com.intuit.spring.pulsar.client.annotations.consumer.PulsarConsumer;
 import com.intuit.spring.pulsar.client.annotations.consumer.Subscription;
 import com.intuit.spring.pulsar.client.annotations.consumer.Topic;
 import com.intuit.spring.pulsar.client.aspect.PulsarConsumerAction;
-import com.intuit.spring.pulsar.client.consumer.listener.IPulsarListener;
 import kotlin.text.Charsets;
 import org.apache.pulsar.client.api.Consumer;
 import org.apache.pulsar.client.api.Message;
@@ -28,11 +27,11 @@ public class SampleConsumer implements MessageListener {
     @Override
     @PulsarConsumerAction(action = "this is my consumer BL")
     public void received(Consumer consumer, Message message) {
-        String messageString = new String((byte[])message.getValue(), Charsets.UTF_8);
+        String messageString = new String((byte[]) message.getValue(), Charsets.UTF_8);
         System.out.println("[RECEIVED] " + message.getMessageId() + " -> " + messageString);
 
         //throwing sample exception to demo exception handling on consumer side
-        if(messageString.length() > 10) {
+        if (messageString.length() > 10) {
             throw new LongMessageConsumerException(messageString);
         }
 
